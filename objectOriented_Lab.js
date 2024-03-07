78007ff0e97639bcf0b34704f85a12667ec12b8c
Dionna <businessladythick@yahoo.com>
2024-03-07 17:47:46 -0500

    added sub-object to Leo called belongings

--- objectOriented_Lab.js
+++ objectOriented_Lab.js
@@ -2,7 +2,6 @@ const adventurer = {
     name: "Robin",
     health: 10,
     inventory: ["sword", "portion", "artifact"],
-}
 
 //this wont physically show anything in the terminal
 // adventurer.inventory[0]
@@ -15,5 +14,13 @@ const adventurer = {
 //adding a nested object loop (to the adventurer object) named companion
 companion: {
     name: "Leo",
-    type; "Cat"
-}
+    type: "Cat",
+
+//adding sub-object to Leo named belongings
+belongings: {
+    name: "Frank",
+    type: "Flea",
+    items: ["small hat", "sunglasses"]
+    }
+  }
+};
