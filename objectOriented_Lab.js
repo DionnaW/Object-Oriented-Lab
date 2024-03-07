289226c74f50ef64053670fc2ca27e5804d8a5d3
Dionna <businessladythick@yahoo.com>
2024-03-07 18:29:24 -0500

    added roll method to the adventurer obj

--- objectOriented_Lab.js
+++ objectOriented_Lab.js
@@ -22,5 +22,12 @@ belongings: {
     type: "Flea",
     items: ["small hat", "sunglasses"]
     }
+  },
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`)
+    //when i tried to run this there was no opuput and i need to know why
   }
 };
+adventurer.roll()
+//this output said Robin rolled a 19, then 13, then 2, then 20
