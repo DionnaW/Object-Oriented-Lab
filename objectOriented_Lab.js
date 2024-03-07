0a66c3dd90914079850f269ae0738f7cf1dfd29e
Dionna <businessladythick@yahoo.com>
2024-03-07 17:40:10 -0500

    added foe loop for inventory and another property

---
+++ objectOriented_Lab.js
@@ -0,0 +1,19 @@
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "portion", "artifact"],
+}
+
+//this wont physically show anything in the terminal
+// adventurer.inventory[0]
+
+//for loop to log each iten in her inventory
+// for (let i = 0; i < adventurer.inventory.length; i++) {
+    // console.log(adventurer.inventory[i]);
+// }
+
+//adding a nested object loop (to the adventurer object) named companion
+companion: {
+    name: "Leo",
+    type; "Cat"
+}
